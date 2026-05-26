ef1df712a19c25a88e270f133f1f5338df91ef72
iThorgrim <contact@warlog.dev>
2026-05-26 07:16:41 +0200

    Refactor SQL insert statements for dungeon templates and spell learning
    - Updated the `lfg_dungeon_template` insert statements to remove the `world` schema prefix for consistency.
    - Modified the `spell_learn_spell` insert statement to also remove the `world` schema prefix, ensuring proper database referencing.

--- sql/updates/world/2026_05_20_00_world.sql
+++ sql/updates/world/2026_05_20_00_world.sql
@@ -1,2 +1,2 @@
 -- When you learn battle pet training you now learn revive battle pets
-INSERT INTO `world`.`spell_learn_spell` (`entry`, `SpellID`, `Active`) VALUES (119467, 125439, 1);
+INSERT INTO `spell_learn_spell` (`entry`, `SpellID`, `Active`) VALUES (119467, 125439, 1);
